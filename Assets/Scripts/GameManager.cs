3e93dd55bc7ea145f1a3c8b00382ffc90931d7ef
Charles Stewart Merriam <159678108+Cstewmer@users.noreply.github.com>
2024-04-20 11:02:36 -0600

    Made a rudimentary score counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,8 +17,8 @@ public class GameManager : MonoBehaviour
 
     public Transform moneys;
 
-    private int CopScore = 0;
-    private int RobberScore = 0;
+    public int CopScore = 0;
+    public int RobberScore = 0;
 
 
 
